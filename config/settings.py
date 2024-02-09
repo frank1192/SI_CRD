a9b6a25baf4f5f1f74e11d6f9e56b6471d9d046f
Frankdvop <franklin.aguirre@correounivalle.edu.co>
2024-02-09 00:46:50 -0500

    prueba solucionando error 6

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*4!i-!t1=5s&^4*y@cruda=)ls3&tjq*&zt0nx$vf5_mmow-2y'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['uv-deporte-formativo.up.railway.app','127.0.0.1']
 
